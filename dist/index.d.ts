c1bb5789d0569902306bdaccf59e83b8c3d278e5
akhilDnT <akhil.j@dntinfotech.com>
2023-05-18 18:10:39 +0530

    updated Dist folder

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,7 @@
-import { DataSource } from "typeorm";
-export declare const AppDataSource: DataSource;
+export declare class Database {
+    dbconnection: any;
+    createDBconnection(): Promise<void>;
+    getEntity(tableName: any): Promise<any>;
+    saveEntity(modelName: any, data: object): Promise<any>;
+    deleteEntity(modelName: any): Promise<any>;
+}
--- dist/index.js
+++ dist/index.js
@@ -1,64 +1,76 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppDataSource = void 0;
+exports.Database = void 0;
 const typeorm_1 = require("typeorm");
 const FileMaster_1 = require("./models/FileMaster");
 const FileProcessDetails_1 = require("./models/FileProcessDetails");
 const FileTypes_1 = require("./models/FileTypes");
 const Operations_1 = require("./models/Operations");
 const Projects_1 = require("./models/Projects");
-exports.AppDataSource = new typeorm_1.DataSource({
-    type: "mysql",
-    host: "35.228.4.120",
-    //   host: "0.0.0.0",
-    port: 3306,
-    username: "root",
-    password: "Admin@123",
-    database: "gedms",
-    synchronize: true,
-    logging: true,
-    entities: [FileMaster_1.filemaster, FileProcessDetails_1.fileprocessdetails, FileTypes_1.filetypes, Operations_1.operations, Projects_1.projects],
-    subscribers: [],
-    migrations: [],
-});
-// console.log("this is datasource", DataSource, AppDataSource)
-exports.AppDataSource.initialize()
-    .then(() => {
-    console.log("Data Source has been initialized!");
-})
-    .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-});
-let getEntity = async (tableName) => {
-    try {
-        let entityData = exports.AppDataSource.getRepository(tableName);
-        return entityData;
+class Database {
+    // console.log("this is datasource", DataSource, AppDataSource)
+    async createDBconnection() {
+        this.dbconnection = new typeorm_1.DataSource({
+            type: "mysql",
+            host: "35.228.4.120",
+            //   host: "0.0.0.0",
+            port: 3306,
+            username: "root",
+            password: "Admin@123",
+            database: "gedms",
+            synchronize: true,
+            logging: true,
+            entities: [
+                FileMaster_1.filemaster,
+                FileProcessDetails_1.fileprocessdetails,
+                FileTypes_1.filetypes,
+                Operations_1.operations,
+                Projects_1.projects,
+            ],
+            subscribers: [],
+            migrations: [],
+        });
+        this.dbconnection
+            .initialize()
+            .then(() => {
+            console.log("Data Source has been initialized!");
+        })
+            .catch((err) => {
+            console.error("Error during Data Source initialization", err);
+        });
     }
-    catch (error) {
-        console.log("Connection to repository is failed");
-        return error;
+    async getEntity(tableName) {
+        try {
+            let entityData = this.dbconnection.getRepository(tableName);
+            return entityData;
+        }
+        catch (error) {
+            console.log("Connection to repository is failed");
+            return error;
+        }
     }
-};
-let saveEntity = async (modelName, data) => {
-    try {
-        let saveRepository = await modelName.save(data);
-        return saveRepository;
+    async saveEntity(modelName, data) {
+        try {
+            let saveRepository = await modelName.save(data);
+            return saveRepository;
+        }
+        catch (error) {
+            console.log("Save Entity failed");
+            return error;
+        }
     }
-    catch (error) {
-        console.log("Save Entity failed");
-        return error;
+    async deleteEntity(modelName) {
+        try {
+            let removedData = await modelName.remove();
+            return removedData;
+        }
+        catch (error) {
+            console.log("Debug Entity failed");
+            return error;
+        }
     }
-};
-let deleteEntity = async (modelName) => {
-    try {
-        let removedData = await modelName.remove();
-        return removedData;
-    }
-    catch (error) {
-        console.log("Debug Entity failed");
-        return error;
-    }
-};
+}
+exports.Database = Database;
 // let updateEntity = async(data: Object) =>{
 //     try {
 //       let updateRepository = await AppDataSource.findOne({data});
